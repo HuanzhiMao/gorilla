c647b3d06dba2be3c72d0a5d953d7077641472b6
Huanzhi (Hans) Mao <huanzhimao@gmail.com>
2024-10-25 18:04:17 -0700

    fix func source code

--- berkeley-function-call-leaderboard/bfcl/eval_checker/multi_turn_eval/func_source_code/vehicle_control.py
+++ berkeley-function-call-leaderboard/bfcl/eval_checker/multi_turn_eval/func_source_code/vehicle_control.py
@@ -182,7 +182,7 @@ class VehicleControlAPI:
                     ]
                 )
             }
-        if self.brakeStatus == "released":
+        if self.brakeStatus == "engage":
             return {"error": "Press the parking brake before starting the engine."}
         if self.fuelLevel < MIN_FUEL_LEVEL:
             return {"error": "Fuel tank is empty."}
