284743da4aaf1174098a6a4138d8b3c8011f021e
Huanzhi (Hans) Mao <huanzhimao@gmail.com>
2024-12-28 12:19:37 +0800

    1

--- berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/qwen_fc.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/qwen_fc.py
@@ -26,7 +26,7 @@ class QwenFCHandler(OSSHandler):
         if type(result) != list:
             return []
         return convert_to_function_call(result)
-    
+
     @override
     def _format_prompt(self, messages, function):
         """
@@ -85,64 +85,6 @@ class QwenFCHandler(OSSHandler):
         {%- if add_generation_prompt %}
             {{- '<|im_start|>assistant\n' }}
         {%- endif %}
-
-
-
-        {%- if tools %}
-            {{- '<|im_start|>system\n' }}
-            {%- if messages[0]['role'] == 'system' %}
-                {{- messages[0]['content'] }}
-            {%- else %}
-                {{- 'You are a helpful and harmless assistant. You are Qwen developed by Alibaba. You should think step-by-step.' }}
-            {%- endif %}
-            {{- "\n\n# Tools\n\nYou may call one or more functions to assist with the user query.\n\nYou are provided with function signatures within <tools></tools> XML tags:\n<tools>" }}
-            {%- for tool in tools %}
-                {{- "\n" }}
-                {{- tool | tojson }}
-            {%- endfor %}
-            {{- "\n</tools>\n\nFor each function call, return a json object with function name and arguments within <tool_call></tool_call> XML tags:\n<tool_call>\n{\"name\": <function-name>, \"arguments\": <args-json-object>}\n</tool_call><|im_end|>\n" }}
-        {%- else %}
-            {%- if messages[0]['role'] == 'system' %}
-                {{- '<|im_start|>system\n' + messages[0]['content'] + '<|im_end|>\n' }}
-            {%- else %}
-                {{- '<|im_start|>system\nYou are a helpful and harmless assistant. You are Qwen developed by Alibaba. You should think step-by-step.<|im_end|>\n' }}
-            {%- endif %}
-        {%- endif %}
-        {%- for message in messages %}
-            {%- if (message.role == "user") or (message.role == "system" and not loop.first) or (message.role == "assistant" and not message.tool_calls) %}
-                {{- '<|im_start|>' + message.role + '\n' + message.content + '<|im_end|>' + '\n' }}
-            {%- elif message.role == "assistant" %}
-                {{- '<|im_start|>' + message.role }}
-                {%- if message.content %}
-                    {{- '\n' + message.content }}
-                {%- endif %}
-                {%- for tool_call in message.tool_calls %}
-                    {%- if tool_call.function is defined %}
-                        {%- set tool_call = tool_call.function %}
-                    {%- endif %}
-                    {{- '\n<tool_call>\n{"name": "' }}
-                    {{- tool_call.name }}
-                    {{- '", "arguments": ' }}
-                    {{- tool_call.arguments | tojson }}
-                    {{- '}\n</tool_call>' }}
-                {%- endfor %}
-                {{- '<|im_end|>\n' }}
-            {%- elif message.role == "tool" %}
-                {%- if (loop.index0 == 0) or (messages[loop.index0 - 1].role != "tool") %}
-                    {{- '<|im_start|>user' }}
-                {%- endif %}
-                {{- '\n<tool_response>\n' }}
-                {{- message.content }}
-                {{- '\n</tool_response>' }}
-                {%- if loop.last or (messages[loop.index0 + 1].role != "tool") %}
-                    {{- '<|im_end|>\n' }}
-                {%- endif %}
-            {%- endif %}
-        {%- endfor %}
-        {%- if add_generation_prompt %}
-            {{- '<|im_start|>assistant\n' }}
-        {%- endif %}
-
         """
         formatted_prompt = ""
 
@@ -220,8 +162,7 @@ class QwenFCHandler(OSSHandler):
                 "tool_calls": extracted_tool_calls,
             }
             model_responses = [
-                {item["name"]: item["arguments"]}
-                for item in extracted_tool_calls
+                {item["name"]: item["arguments"]} for item in extracted_tool_calls
             ]
         else:
             model_responses_message_for_chat_history = {
