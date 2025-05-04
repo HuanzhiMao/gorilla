54593d8e75577a55eea1646c8d570fabcd902415
itea1001 <mingxuanl@uchicago.edu>
2025-05-03 19:46:08 -0500

    fix is_fc_model flag in model config (based on handler init)

--- berkeley-function-call-leaderboard/bfcl/constants/model_config.py
+++ berkeley-function-call-leaderboard/bfcl/constants/model_config.py
@@ -996,7 +996,7 @@ local_inference_model_map = {
         model_handler=SalesforceLlamaHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "Salesforce/Llama-xLAM-2-8b-fc-r": ModelConfig(
@@ -1008,7 +1008,7 @@ local_inference_model_map = {
         model_handler=SalesforceLlamaHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "Salesforce/xLAM-2-32b-fc-r": ModelConfig(
@@ -1020,7 +1020,7 @@ local_inference_model_map = {
         model_handler=SalesforceQwenHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "Salesforce/xLAM-2-3b-fc-r": ModelConfig(
@@ -1032,7 +1032,7 @@ local_inference_model_map = {
         model_handler=SalesforceQwenHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "Salesforce/xLAM-2-1b-fc-r": ModelConfig(
@@ -1044,7 +1044,7 @@ local_inference_model_map = {
         model_handler=SalesforceQwenHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "mistralai/Ministral-8B-Instruct-2410": ModelConfig(
@@ -1056,7 +1056,7 @@ local_inference_model_map = {
         model_handler=MistralFCHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "microsoft/phi-4": ModelConfig(
@@ -1104,7 +1104,7 @@ local_inference_model_map = {
         model_handler=GraniteHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=True,
     ),
     "MadeAgents/Hammer2.1-7b": ModelConfig(
@@ -1116,7 +1116,7 @@ local_inference_model_map = {
         model_handler=HammerHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "MadeAgents/Hammer2.1-3b": ModelConfig(
@@ -1128,7 +1128,7 @@ local_inference_model_map = {
         model_handler=HammerHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "MadeAgents/Hammer2.1-1.5b": ModelConfig(
@@ -1140,7 +1140,7 @@ local_inference_model_map = {
         model_handler=HammerHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "MadeAgents/Hammer2.1-0.5b": ModelConfig(
@@ -1152,7 +1152,7 @@ local_inference_model_map = {
         model_handler=HammerHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "THUDM/glm-4-9b-chat": ModelConfig(
@@ -1164,7 +1164,7 @@ local_inference_model_map = {
         model_handler=GLMHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=True,
     ),
     "Qwen/Qwen2.5-0.5B-Instruct-FC": ModelConfig(
@@ -1344,7 +1344,7 @@ local_inference_model_map = {
         model_handler=LlamaHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "openbmb/MiniCPM3-4B": ModelConfig(
@@ -1380,7 +1380,7 @@ local_inference_model_map = {
         model_handler=LlamaHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "watt-ai/watt-tool-70B": ModelConfig(
@@ -1392,7 +1392,7 @@ local_inference_model_map = {
         model_handler=LlamaHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
     "ZJared/Haha-7B": ModelConfig(
@@ -1548,7 +1548,7 @@ local_inference_model_map = {
         model_handler=ThinkAgentHandler,
         input_price=None,
         output_price=None,
-        is_fc_model=True,
+        is_fc_model=False,
         underscore_to_dot=False,
     ),
 }
