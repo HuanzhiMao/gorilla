9c500828799cbb2f060ba5a4efd3c2a2d423c931
Huanzhi Mao <huanzhimao@gmail.com>
2026-02-20 01:36:53 -0800

    1

--- berkeley-function-call-leaderboard/bfcl_eval/constants/category_mapping.py
+++ berkeley-function-call-leaderboard/bfcl_eval/constants/category_mapping.py
@@ -1,13 +1,9 @@
-
 VERSION_PREFIX = "BFCL_v4"
 
-ALL_AVAILABLE_MEMORY_BACKENDS = [
-    "kv",
-    "vector",
-    "rec_sum",
-]
 
-NON_LIVE_CATEGORY = [
+#### Text Modality ####
+
+TEXT_NON_LIVE_CATEGORY = [
     "text:simple_python",
     "text:simple_java",
     "text:simple_javascript",
@@ -23,7 +19,7 @@ NON_LIVE_CATEGORY = [
     # "text:sql",
     # "text:chatable",
 ]
-LIVE_CATEGORY = [
+TEXT_LIVE_CATEGORY = [
     "text:live_simple",
     "text:live_multiple",
     "text:live_parallel",
@@ -31,33 +27,36 @@ LIVE_CATEGORY = [
     "text:live_irrelevance",
     "text:live_relevance",
 ]
-MULTI_TURN_CATEGORY = [
+TEXT_MULTI_TURN_CATEGORY = [
     "text:multi_turn_base",
     "text:multi_turn_miss_func",
     "text:multi_turn_miss_param",
     "text:multi_turn_long_context",
     # "text:multi_turn_composite",
 ]
-WEB_SEARCH_CATEGORY = [
+TEXT_WEB_SEARCH_CATEGORY = [
     "text:web_search_base",
     "text:web_search_no_snippet",
 ]
-VISION_CATEGORY = [
-    # "vision:vision_base",
-    # @HuanzhiMao FIXME: uncomment these
-    # "vision:vision_crop_169",
-    # "vision:vision_crop_43",
-    # "vision:vision_resize_169",
-    # "vision:vision_resize_43",
-    # "vision:vision_bw",
-    # "vision:vision_edge",
-    # "vision:vision_rg",
-    "vision:geogesser_type1",
-    "vision:geogesser_type2",
-    "vision:geogesser_type3",
+VISION_WEB_SEARCH_CATEGORY = [
+    "vision:web_search_base",
+    "vision:web_search_crop_169",
+    "vision:web_search_crop_43",
+    "vision:web_search_resize_169",
+    "vision:web_search_resize_43",
+    "vision:web_search_bw",
+    "vision:web_search_edge",
+    "vision:web_search_rg",
 ]
 
-MEMORY_CATEGORY = [f"text:memory_{backend}" for backend in ALL_AVAILABLE_MEMORY_BACKENDS]
+ALL_AVAILABLE_MEMORY_BACKENDS = [
+    "kv",
+    "vector",
+    "rec_sum",
+]
+TEXT_MEMORY_CATEGORY = [
+    f"text:memory_{backend}" for backend in ALL_AVAILABLE_MEMORY_BACKENDS
+]
 MEMORY_SCENARIO_NAME = [
     "student",
     "customer",
@@ -66,59 +65,91 @@ MEMORY_SCENARIO_NAME = [
     "notetaker",
 ]
 
-
-SINGLE_TURN_CATEGORY = NON_LIVE_CATEGORY + LIVE_CATEGORY
-AGENTIC_CATEGORY = MEMORY_CATEGORY + WEB_SEARCH_CATEGORY
+TEXT_SINGLE_TURN_CATEGORY = TEXT_NON_LIVE_CATEGORY + TEXT_LIVE_CATEGORY
+TEXT_AGENTIC_CATEGORY = TEXT_MEMORY_CATEGORY + TEXT_WEB_SEARCH_CATEGORY
 NON_SCORING_CATEGORY = ["text:format_sensitivity"]
 
-# Audio reuses the same underlying tests as text, delivered via different modalities.
-AUDIO_CATEGORY = [
-    cat.replace("text:", "true_audio:")
-    for cat in SINGLE_TURN_CATEGORY + MULTI_TURN_CATEGORY
-]
-AUDIO_TRANSCRIPT_CATEGORY = [
-    cat.replace("text:", "text_audio:")
-    for cat in SINGLE_TURN_CATEGORY + MULTI_TURN_CATEGORY
+#### Vision Modality ####
+
+VISION_GEOGESSER_CATEGORY = [
+    "vision:geogesser_type1",
+    "vision:geogesser_type2",
+    "vision:geogesser_type3",
 ]
 
-ALL_SCORING_CATEGORIES = (
-    SINGLE_TURN_CATEGORY
-    + MULTI_TURN_CATEGORY
-    + AGENTIC_CATEGORY
-    + VISION_CATEGORY
-    + AUDIO_CATEGORY
-    + AUDIO_TRANSCRIPT_CATEGORY
+#### True Audio Modality ####
+
+# Audio reuses the same underlying tests as text, delivered via different modalities.
+TRUE_AUDIO_NON_LIVE_CATEGORY = [
+    cat.replace("text:", "true_audio:") for cat in TEXT_NON_LIVE_CATEGORY
+]
+TRUE_AUDIO_LIVE_CATEGORY = [
+    cat.replace("text:", "true_audio:") for cat in TEXT_LIVE_CATEGORY
+]
+TRUE_AUDIO_SINGLE_TURN_CATEGORY = TRUE_AUDIO_NON_LIVE_CATEGORY + TRUE_AUDIO_LIVE_CATEGORY
+TRUE_AUDIO_MULTI_TURN_CATEGORY = [
+    cat.replace("text:", "true_audio:") for cat in TEXT_MULTI_TURN_CATEGORY
+]
+
+#### Text Audio Modality ####
+
+TEXT_AUDIO_NON_LIVE_CATEGORY = [
+    cat.replace("text:", "text_audio:") for cat in TEXT_NON_LIVE_CATEGORY
+]
+TEXT_AUDIO_LIVE_CATEGORY = [
+    cat.replace("text:", "text_audio:") for cat in TEXT_LIVE_CATEGORY
+]
+TEXT_AUDIO_SINGLE_TURN_CATEGORY = TEXT_AUDIO_NON_LIVE_CATEGORY + TEXT_AUDIO_LIVE_CATEGORY
+TEXT_AUDIO_MULTI_TURN_CATEGORY = [
+    cat.replace("text:", "text_audio:") for cat in TEXT_MULTI_TURN_CATEGORY
+]
+
+
+#### Aggregate categories ####
+ALL_TEXT_SCORING_CATEGORIES = (
+    TEXT_SINGLE_TURN_CATEGORY + TEXT_MULTI_TURN_CATEGORY + TEXT_AGENTIC_CATEGORY
 )
-ALL_CATEGORIES = ALL_SCORING_CATEGORIES + NON_SCORING_CATEGORY
+ALL_TEXT_CATEGORIES = ALL_TEXT_SCORING_CATEGORIES + NON_SCORING_CATEGORY
+ALL_VISION_CATEGORY = VISION_WEB_SEARCH_CATEGORY + VISION_GEOGESSER_CATEGORY
+ALL_TRUE_AUDIO_CATEGORY = TRUE_AUDIO_SINGLE_TURN_CATEGORY + TRUE_AUDIO_MULTI_TURN_CATEGORY
+ALL_TEXT_AUDIO_CATEGORY = TEXT_AUDIO_SINGLE_TURN_CATEGORY + TEXT_AUDIO_MULTI_TURN_CATEGORY
+
+ALL_MODALITY_SCORING_CATEGORIES = (
+    ALL_TEXT_SCORING_CATEGORIES
+    + ALL_TRUE_AUDIO_CATEGORY
+    + ALL_TEXT_AUDIO_CATEGORY
+    + ALL_VISION_CATEGORY
+)
+ALL_MODALITY_CATEGORIES = ALL_MODALITY_SCORING_CATEGORIES + NON_SCORING_CATEGORY
 
 TEST_COLLECTION_MAPPING = {
-    "all": ALL_CATEGORIES,
-    "all_scoring": ALL_SCORING_CATEGORIES,
-    "multi_turn": MULTI_TURN_CATEGORY,
-    "single_turn": SINGLE_TURN_CATEGORY,
-    "live": LIVE_CATEGORY,
-    "non_live": NON_LIVE_CATEGORY,
-    "non_python": [
-        "text:simple_java",
-        "text:simple_javascript",
-    ],
-    "python": [
-        "text:simple_python",
-        "text:irrelevance",
-        "text:parallel",
-        "text:multiple",
-        "text:parallel_multiple",
-        "text:live_simple",
-        "text:live_multiple",
-        "text:live_parallel",
-        "text:live_parallel_multiple",
-        "text:live_irrelevance",
-        "text:live_relevance",
-    ],
-    "memory": MEMORY_CATEGORY,
-    "web_search": WEB_SEARCH_CATEGORY,
-    "agentic": AGENTIC_CATEGORY,
-    "vision": VISION_CATEGORY,
-    "true_audio": AUDIO_CATEGORY,
-    "text_audio": AUDIO_TRANSCRIPT_CATEGORY,
+    # Aggregate categories
+    "all": ALL_MODALITY_CATEGORIES,
+    "all_scoring": ALL_MODALITY_SCORING_CATEGORIES,
+    "all_text": ALL_TEXT_CATEGORIES,
+    "all_text_scoring": ALL_TEXT_SCORING_CATEGORIES,
+    "all_true_audio": ALL_TRUE_AUDIO_CATEGORY,
+    "all_text_audio": ALL_TEXT_AUDIO_CATEGORY,
+    "all_vision": ALL_VISION_CATEGORY,
+    # Text categories
+    "text_non_live": TEXT_NON_LIVE_CATEGORY,
+    "text_live": TEXT_LIVE_CATEGORY,
+    "text_single_turn": TEXT_SINGLE_TURN_CATEGORY,
+    "text_multi_turn": TEXT_MULTI_TURN_CATEGORY,
+    "text_agentic": TEXT_AGENTIC_CATEGORY,
+    "text_web_search": TEXT_WEB_SEARCH_CATEGORY,
+    "text_memory": TEXT_MEMORY_CATEGORY,
+    # Vision categories
+    "vision_web_search": VISION_WEB_SEARCH_CATEGORY,
+    "vision_geogesser": VISION_GEOGESSER_CATEGORY,
+    # True Audio categories
+    "true_audio_non_live": TRUE_AUDIO_NON_LIVE_CATEGORY,
+    "true_audio_live": TRUE_AUDIO_LIVE_CATEGORY,
+    "true_audio_single_turn": TRUE_AUDIO_SINGLE_TURN_CATEGORY,
+    "true_audio_multi_turn": TRUE_AUDIO_MULTI_TURN_CATEGORY,
+    # Text Audio categories
+    "text_audio_non_live": TEXT_AUDIO_NON_LIVE_CATEGORY,
+    "text_audio_live": TEXT_AUDIO_LIVE_CATEGORY,
+    "text_audio_single_turn": TEXT_AUDIO_SINGLE_TURN_CATEGORY,
+    "text_audio_multi_turn": TEXT_AUDIO_MULTI_TURN_CATEGORY,
 }
