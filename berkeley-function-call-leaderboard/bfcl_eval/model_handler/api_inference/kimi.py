d20f08b5c86166d7d5e203770fa0584d8480448d
Huanzhi Mao <huanzhimao@gmail.com>
2026-04-18 13:37:40 -0700

    1

--- berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/kimi.py
+++ berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/kimi.py
@@ -1,6 +1,11 @@
 import os
-from bfcl_eval.model_handler.api_inference.openai_completion import OpenAICompletionsHandler
+
+from bfcl_eval.constants.enums import ResultType
+from bfcl_eval.model_handler.api_inference.openai_completion import (
+    OpenAICompletionsHandler,
+)
 from openai import OpenAI
+from overrides import override
 
 
 class KimiHandler(OpenAICompletionsHandler):
@@ -21,3 +26,38 @@ class KimiHandler(OpenAICompletionsHandler):
             # base_url="https://api.moonshot.cn/v1", 
             api_key=os.getenv("KIMI_API_KEY")
         )
+
+    @override
+    def _add_execution_results_FC(
+        self,
+        inference_data: dict,
+        execution_results: list[dict],
+        model_response_data: dict,
+    ) -> dict:
+        for execution_result, tool_call_id in zip(
+            execution_results, model_response_data["tool_call_ids"]
+        ):
+            if execution_result["result_type"] == ResultType.TEXT:
+                tool_message = {
+                    "role": "tool",
+                    "content": execution_result["result"],
+                    "tool_call_id": tool_call_id,
+                }
+            elif execution_result["result_type"] == ResultType.IMAGE:
+                image_content = execution_result["result"]
+                tool_message = {
+                    "role": "tool",
+                    "tool_call_id": tool_call_id,
+                    "content": [
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:{image_content['type']};base64,{image_content['image_base64']}"
+                            },
+                        }
+                    ],
+                }
+
+            inference_data["message"].append(tool_message)
+
+        return inference_data
